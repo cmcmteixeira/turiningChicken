cb522c5f38ddea4fb7eb50bff16a5759075f87b7
cmcmteixeira <cmcmteixeira@gmail.com>
2016-09-09 17:15:29 +0100

    [Added] Login Screen

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import HelloWorld from './components/HelloWorld/HelloWorld.jsx'
+
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
+import LoginPage from './components/Login/Login.jsx'
+
 import './index.scss'
 
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
 import nameApp from './reducers/nameReducer.js'
 
 var store = createStore(nameApp, {name:'carlos'});
@@ -18,7 +20,7 @@ injectTapEventPlugin();
 ReactDom.render(
     <Provider store={store}>
         <MuiThemeProvider  muiTheme={getMuiTheme()}>
-            <HelloWorld/>
+            <LoginPage/>
         </MuiThemeProvider>
     </Provider>,
     document.getElementById('main'))
